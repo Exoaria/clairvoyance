d750b2e7a9727f020836825e87125424a2cb37c1
Janus Reith <janus.reith@googlemail.com>
2015-08-12 19:45:05 +0200

    Added Settings pane

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -4,3 +4,8 @@ func _on_MenuButton_3_pressed():
 
 func _on_MenuButton_pressed():
 	get_tree().change_scene("res://Scenes/Map1.scn")
+
+
+func _on_MenuButton_2_pressed():
+	pass # replace with function body
+	get_tree().change_scene("res://Scenes/Settings.scn")
