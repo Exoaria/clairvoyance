e825ef27ec090a5fc36e573c97ec4279b3006665
Janus Reith <janus.reith@googlemail.com>
2015-08-13 16:13:48 +0200

    Implemented Fullscreen-Checkbox. Need to find out how to store game settings.
    Signed-off-by: Janus Reith <janus.reith@googlemail.com>

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -2,3 +2,13 @@
 
 func _on_MenuButton_pressed():
 	get_tree().change_scene("res://Scenes/menu.scn")
+
+
+func _on_CheckBox_toggled( pressed ):
+	if (pressed):
+		OS.set_window_fullscreen(true)
+	else:
+		OS.set_window_fullscreen(false)
+
+
+
